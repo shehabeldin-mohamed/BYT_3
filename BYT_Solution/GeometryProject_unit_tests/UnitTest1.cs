d29b70750ab0a4a5a42f161faca0dfd3a877c46e
shehabkh03 <shehabkh249@gmail.com>
2025-10-31 17:19:27 +0100

    Added UnitTests for a few shapes

---
+++ BYT_Solution/GeometryProject_unit_tests/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using BYT_Solution.Models;
+
+namespace GeometryProject_unit_tests;
+
+public class Tests
+{
+    private readonly IShape Cube = new Cube(5);
+    private readonly IShape Cylinder = new Cylinder(5,5);
+    private readonly IShape Rectangle = new Rectangle(5,3);
+    private readonly IShape sphere = new Sphere(5);
+    
+    //Cube Tests
+    
+    
+    //Cylider Tests
+    
+    
+    
+    //Rectangles Tests
+    
+    
+    
+    //Sphere Tests
+    [Test]
+    public void TestSphereCalculateArea()
+    {
+        Assert.That(sphere.CalculateArea(), Is.EqualTo(314.159).Within(0.001));
+    }
+
+    [Test]
+    public void TestSphereCalculateVolume()
+    {
+        Assert.That(sphere.CalculateVolume(), Is.EqualTo(523.598).Within(0.001));
+    }
+    
+
+}
